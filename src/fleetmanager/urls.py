2d16e9926dbaaccf2ff2d947c5f09948a61d4790
subajat1 <bayumunajat@outlook.com>
2019-02-24 16:46:16 +0800

    clearing

--- src/fleetmanager/urls.py
+++ src/fleetmanager/urls.py
@@ -13,9 +13,3 @@ urlpatterns = [
     url(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
     url(r'^admin/', admin.site.urls),
 ]
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns += [
-#         url(r'^__debug__/', include(debug_toolbar.urls)),
-#     ]
